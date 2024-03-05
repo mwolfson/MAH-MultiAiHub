a1e1fd6c34e50be7e925a800d4fe7d35c9a29c5d
Mike Wolfson <mwolfson@gmail.com>
2024-03-04 19:12:10 -0700

    Initial repository creation

---
+++ MultiAiHub.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+
+# ## MAH - Multi AI Hub
+# 
+# This project is designed to make it easy to send the same prompt to multiple LLMs which is useful for testing and comparison.
+# 
+# ### API Access Required
+# 
+# You must have access to the services (Currently Anthropic, Google, OpenAI, and Perplexity) in order to use them in this script.
+
+# ### Working with API keys
+# 
+# Set the API keys as a system variables.
+# 
+# - [Setting an Environment Variable on Mac/Linux](https://phoenixnap.com/kb/set-environment-variable-mac)
+# - [Setting an Environment Variable on Windows](https://phoenixnap.com/kb/windows-set-environment-variable)
+
+# #### Import Google Generative GenerativeAI library and set API Key
+# 
+# "You will need to set the Gemini API key as a system variable named: `GOOGLE_API_KEY`.  \n"
+
+# ## Tools to Get Environment Variables from OS
+# 
+# PIP Install:
+# 
+# `pip install python-dotenv`
+
+# In[ ]:
+
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+# ## Setup Google GenAI
+# 
+# PIP Install: 
+# 
+# `pip install -q google.generativeai`
+
+# In[ ]:
+
+
+import google.generativeai as googleai
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+apiKey = os.getenv('GOOGLE_API_KEY')
+
+googleai.configure(api_key=apiKey,
+               transport="rest",
+    )
+
+
+# ## Explore the Available Models
+# 
+# Learn which models are currently available
+# 
+
+# In[ ]:
+
+
+# for m in googleai.list_models():
+#     print(f"name: {m.name}")
+#     print(f"description: {m.description}")
+#     print(f"generation methods:{m.supported_generation_methods}\n")
+
+
+# ### Filter models to ensure model we want is supported
+# - `generateContent` is the value we are looking for
+
+# In[ ]:
+
+
+# for m in googleai.list_models():
+#   if 'generateContent' in m.supported_generation_methods:
+#     print(m.name)
+
+
+# ### Google AI Helper Function
+# 
+# - The `@retry` decorator helps you to retry the API call if it fails.
+
+# In[ ]:
+
+
+from google.api_core import retry
+@retry.Retry()
+def generate_text_google(prompt, model):
+    model = googleai.GenerativeModel(model)
+    response = model.generate_content(prompt)
+    return response.text
+
+
+# ### Test **Google AI Helper** function
+
+# In[ ]:
+
+
+# print(generate_text_google("Thursday evenings are perfect for", "gemini-pro"))
+
+
+# ## Setup Open AI APIs
+# 
+# ```
+# OpenAI's APIs offer developers the ability to integrate advanced artificial intelligence capabilities into their applications, enabling a wide range of tasks from text generation to complex problem-solving.
+# ```
+# Documentation: [https://beta.openai.com/docs/](https://beta.openai.com/docs/)
+# 
+# ### Obtaining API Keys:
+# - **OpenAI Platform**: [https://platform.openai.com/](https://platform.openai.com/)
+#   - After signing up or logging in, navigate to the API section to manage and obtain your API keys.
+# - You will need to set the OpenAI API key as a system variable named: `OPENAI_API_KEY`.  
+# 
+# Note: do NOT check your API key into a public Github repo, or it will get revoked 
+#   
+#   
+# 
+
+# In[ ]:
+
+
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+
+# ### Open AI Helper Function
+# 
+# PIP Dependencies:
+# 
+# `pip install --upgrade openai`
+
+# In[ ]:
+
+
+from openai import OpenAI
+client = OpenAI()
+
+def generate_text_openai(pre, prompt, model):
+    completion = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": pre},
+        {"role": "user", "content": prompt}
+    ]
+    )
+
+    return completion.choices[0].message.content
+
+
+# ## Test **Open AI Helper** Function
+
+# In[ ]:
+
+
+# print(generate_text_openai("You are a pirate", "Thursday evenings are perfect for", "gpt-3.5-turbo"))
+
+
+# ## Setup Perplexity API
+# 
+# You will need a key set to `PERPLEXITY_API_KEY`
+
+# In[ ]:
+
+
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+YOUR_API_KEY = os.getenv('PERPLEXITY_API_KEYXXX')
+
+
+# ## Perplexity Helper function
+# 
+# No PIP dependency
+
+# In[ ]:
+
+
+from openai import OpenAI
+
+perplexityClient = OpenAI(api_key=YOUR_API_KEY, base_url="https://api.perplexity.ai")
+
+def generate_text_perplexity(system, user, model):
+    response = perplexityClient.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": system},
+        {"role": "user", "content": user}
+    ]
+    )
+
+    content = response.choices[0].message.content
+    return content
+
+
+# ## Test **Perplexity Helper** Function
+
+# In[ ]:
+
+
+# print(generate_text_perplexity("you are a pirate", "say hello and return the message in uppercase", "mistral-7b-instruct"))
+
+
+# ## Setup Anthropic
+# 
+# Check the [docs](https://github.com/anthropics/anthropic-sdk-python), and get an [API Key](https://console.anthropic.com/dashboard)
+# 
+# ### Import SDK 
+# 
+# PIP Install:
+# 
+# `pip install anthropic`
+# 
+
+# In[ ]:
+
+
+from anthropic import Anthropic
+
+anthropic_client = Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+
+def generate_text_anthropic(user, model="claude-3-opus-20240229"):
+    response = anthropic_client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": user,
+            }
+        ],
+        model=model,
+        )
+    content = response.content[0].text 
+    return content
+
+
+# ### Test the Anthropic API directly
+
+# In[ ]:
+
+
+# print(generate_text_anthropic("you are a pirate" + "say hello and return the message in uppercase", "claude-3-opus-20240229"))
+
+
+# ## Proxy to Previously Defined Functions
+
+# 1. Define a function for each model you want to test
+# 2. Create a constant to reference that model
+# 3. Add both to the dictionary
+
+# In[ ]:
+
+
+# Each model has a function that translates the standard input to match the model's expected input format
+def action_anthropic_opus(system, user, format):
+    response = generate_text_anthropic(system + user + format, "claude-3-opus-20240229")
+    return response
+
+def action_anthropic_sonnet(system, user, format):
+    response = generate_text_anthropic(system + user + format, "claude-3-sonnet-20240229")
+    return response
+
+def action_gemini_pro(system, user, format,):
+    response = generate_text_google(system + user + format, "gemini-pro")
+    return response
+
+def action_openai_35turbo(system, user, format):
+    response = generate_text_openai(system, user + format, "gpt-3.5-turbo")
+    return response
+
+def action_openai_gpt4(system, user, format):
+    response = generate_text_openai(system, user + format, "gpt-4")
+    return response
+
+def action_openai_gpt4_preview(system, user, format):
+    response = generate_text_openai(system, user + format, "gpt-4-0125-preview")
+    return response
+
+def action_mistral_7b(system, user, format):
+    response = generate_text_perplexity(system, user + format, "mistral-7b-instruct")
+    return response
+
+def action_mixtral_8x7b(system, user, format):
+    response = generate_text_perplexity(system, user + format, "mixtral-8x7b-instruct")
+    return response
+
+def action_sonar_medium_online(system, user, format):
+    response = generate_text_perplexity("", system + user + format, "sonar-medium-online")
+    return response
+
+# Constants for the models
+ANTHROPIC_OPUS = "claude-3-opus-20240229"
+ANTHROPIC_SONNET = "claude-3-sonnet-20240229"
+GEMINI_PRO = "gemini-pro"
+OPEN_AI_GPT35TURBO = "gpt-3.5-turbo"
+OPEN_AI_GPT4 = "gpt-4"
+OPEN_AI_GPT4PREVIEW = "gpt-4-0125-preview"
+MISTRAL_7B = "mistral-7b-instruct"
+MIXTRAL_8X7B = "mixtral-8x7b-instruct"
+SONAR_MED_ONLINE = "sonar-medium-online"
+
+# Dictionary mapping models to their respective functions
+action_dict = {
+    ANTHROPIC_OPUS: action_anthropic_opus,
+    ANTHROPIC_SONNET: action_anthropic_sonnet,
+    GEMINI_PRO: action_gemini_pro,
+    OPEN_AI_GPT35TURBO: action_openai_35turbo,
+    OPEN_AI_GPT4: action_openai_gpt4,
+    OPEN_AI_GPT4PREVIEW: action_openai_gpt4_preview,
+    MISTRAL_7B: action_mistral_7b,
+    MIXTRAL_8X7B: action_mixtral_8x7b,
+    SONAR_MED_ONLINE: action_sonar_medium_online
+}
+
+        
+
+
+# In[1]:
+
+
+def generate_text(models, system, user, format):
+    """
+    Generate text using the specified models.
+
+    If there is only 1 model in the list, the response will not include the model name.
+    Otherwise, the response will include the model name as a header of the text generated from each model.
+
+    Args:
+        models (list): A list of model names where to run the prompt.
+        system (str): The prompt *system* information to define context.
+        user (str): The prompt *user* information to describe the question to ask.
+        format (str): The prompt desired *format* of the generated text.
+
+    Returns:
+        str: the generated text for all of the models in the input list
+    """
+    output = ""
+    is_single_model = len(models) == 1
+
+    for model in models:
+        action = action_dict.get(model)
+        if action:
+            try:
+                response = action(system=system, user=user, format=format)
+                if not is_single_model:
+                    output += "\n\n# MODEL: " + model + "\n"
+                output += response
+            except Exception as e:
+                if not is_single_model:
+                    output += "\n\n# MODEL: " + model + "\n"
+                output += "Exception" + str(e)
+        else:
+            print("No action defined for model: ", model)
+
+    return output
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,141 @@
-# MultiAiHub
-MAW - Multi AI Hub is a project designed to make it easy to send the same prompt to multiple LLMs to help with testing and comparison.
+# MAH - Multi AI Hub
+**MAH - Multi AI Hub** is a project designed to make it easy to send the same prompt to multiple LLMs to help with testing and comparison.
+
+If you have already setup the SDK for `Anthropic`, `OpenAI`, `Google Gemini` or `Perplexity` and used the default environment variable names for the API keys, you can already use this tool without doing anything else.
+
+## Usage
+
+Copy the `MultiAiHub.py` file from this repository to the same directory of the notebook where you will be using it.
+
+### Install the AI Vendor SDKs and set API Keys
+
+To use this tool, you will need to get API keys from each of the vendors, and set them as environment variables.
+
+Setup information for installing these tools, and setting up the API keys is included in the [MultiAiHub.ipynb](./MultiAiHub.ipynb) notebook. MAH uses standard names for the environment variables, so if your configuration matches the vendor standards, this will work without needing any additional configuration.
+
+## Prompt Multiple AIs at Once
+
+You can issue the same prompt to multiple AIs by giving a list of which models to test, along with the prompt to test with.
+
+```python
+# Import the code
+import MultiAiHub as mah
+
+# List of each model to test the prompt with
+models = [
+    mah.ANTHROPIC_OPUS,
+    mah.ANTHROPIC_SONNET,    
+    mah.GEMINI_PRO,
+    mah.OPEN_AI_GPT35TURBO,
+    mah.OPEN_AI_GPT4,
+    mah.OPEN_AI_GPT4PREVIEW
+    mah.MISTRAL_7B,
+    mah.MIXTRAL_8X7B,
+    mah.SONAR_MED_ONLINE
+]
+
+# Common prompt elements
+system = "You are a pirate"
+user = "Say hello, and ask how my day way"
+instruction = "Format the response as proper JSON"
+
+## Call to generate text
+response = maw.generate_text(models, system, user, instruction)
+print(response)
+```
+
+#### Results from sending list of models
+
+When you run this with more then 1 model, the output will include a header with a name to identify the model used:
+
+`# MODEL: <model name>`
+
+Looking like this:
+
+```python
+# MODEL: claude-3-opus-20240229
+AHOY THERE, MATEY! WELCOME ABOARD THE JOLLY ROGER!
+
+DID YE HEAR ABOUT THE PIRATE WHO COULDN'T FIND HIS TREASURE? HE WAS LOST WITHOUT HIS MAP! HAR HAR HAR!
+
+NOW, WHAT SAY YE? ARE YE READY TO SET SAIL AND SEEK OUT SOME BOOTY?
+
+# MODEL: claude-3-sonnet-20240229
+AHOY LANDLUBBER! ALLOW ME TO REGALE YE WITH A TALE OF BURIED TREASURE. WHY IS A PIRATE'S FAVORITE LETTER THE 'R'? BECAUSE 'TWAS ONCE THE SEA'S GREATEST TREASURE!
+
+# MODEL: gemini-pro
+Ahoy there, matey!
+
+What do you call a pirate who always knows where his treasure is?
+
+A TREASURE MAPPER!
+
+# MODEL: gpt-3.5-turbo
+AVAST, ME HEARTY! WHY DID THE PIRATE GO TO SCHOOL? TO IMPROVE HIS "ARRRR" TICULATION! ARRRRR!
+
+# MODEL: gpt-4
+AHOOY THERE, MATEY! GLAD YER ON BOARD FOR A GOOD LAUGH!
+
+NOW, WHY DON'T PIRATES EVER RULE THE WORLD?
+
+BECAUSE, ONCE THEY HAVE THE TREASURE, THEY CAN'T REMEMBER WHERE THEY BURIED THE 'X!' HAHAHA!
+
+# MODEL: gpt-4-0125-preview
+AHARR MATEY! WELCOME ABOARD!
+
+WANT TO HEAR A JOKE ABOUT TREASURE? WHY DO PIRATES MAKE TERRIBLE SINGERS? BECAUSE THEY CAN HIT THE HIGH SEAS BUT NEVER THE HIGH C'S!
+
+# MODEL: mistral-7b-instruct
+ARRR, ME Hearty Matey! Welcome to me hideaway, buried deep in the heart of the Seven Seas! Here be a wee joke to tickle yer funny bone: Why did the pirate cross the Atlantic? To get to the other ARRR-eas! Aye, a hearty laugh can make even the saltiest sea dog smile. So, grab yer grog and join me in a hearty chuckle!
+
+# MODEL: mixtral-8x7b-instruct
+Ahoy there! Greetings, me hearty! I've a joke for ye about treasure:
+
+Why don't pirates ever get bored of digging for treasure?
+
+Because they always find it an "arrr"-dventure! Yarrr!
+
+# MODEL: sonar-medium-online
+ARRRR! Ahoy there matey! What's better than finding gold on your ship? Finding out it was only fool's gold.
+```
+
+### Prompt Single AI to get Raw result
+
+Once you have run your prompt against multiple LLMs, and decided which one is best, you can get the raw response (*without the header listing the model name*) by just sending a list with only 1 model name
+
+```python
+import MultiAiHub as maw
+
+# Only Gemini Pro in the list
+models = [
+    maw.GEMINI_PRO
+]
+
+# Common prompt elements
+system = "You are a pirate"
+user = "Say hello, and ask how my day way"
+format = "Format the response as proper JSON"
+
+## Call to generate text
+response = maw.generate_text(models, system, user, format)
+print(response)
+```
+#### Results when sending single model
+
+This result is the *raw* content from the API without any extra header or additional content
+
+```python
+Ahoy there, matey!
+
+Why did the pirate bury his treasure on the beach?
+
+Because he heard it was the best place to keep his doubloons hidden!
+```
+
+## Example
+
+Run the [mahDemo.iypnb](./mahDemo.ipynb) to see how this works yourself.
+
+## Adding models to MAH
+
+I wrote this for myself, and I hope you find it useful as well. It is very easy to add more SDKs and models to MAH, checkout the [MultiAiHub.ipynb](./MultiAiHub.ipynb) notebook where the steps are documented.
